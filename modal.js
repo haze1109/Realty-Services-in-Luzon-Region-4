ec3cbd9d4e1dba8dd420787009e11cb56c1616f2
haze1109 <hazemorales11@gmail.com>
2023-04-07 23:28:50 +0800

    modal

--- modal.js
+++ modal.js
@@ -14,3 +14,26 @@ var closeBtn = modal.querySelector(".btn-close");
 closeBtn.addEventListener("click", function() {
   modal.style.display = "none";
 });
+
+
+
+// JavaScript code for the duplicated modal and button
+// Update the IDs in the code to match the duplicated modal and button IDs
+
+// Show modal when button is clicked
+document.getElementById('showFormButton2').addEventListener('click', function() {
+    document.getElementById('googleFormContainer2').style.display = 'block';
+  });
+  
+  // Close modal when close button is clicked
+  document.querySelector('#googleFormContainer2 .btn-close').addEventListener('click', function() {
+    document.getElementById('googleFormContainer2').style.display = 'none';
+  });
+  
+  // Close modal when clicked outside the modal
+  document.addEventListener('click', function(event) {
+    if (event.target === document.getElementById('googleFormContainer2')) {
+      document.getElementById('googleFormContainer2').style.display = 'none';
+    }
+  });
+  

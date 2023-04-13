35cfef374249df7c9f1c7f28f3efff25492b0495
haze1109 <hazemorales11@gmail.com>
2023-04-13 19:42:43 +0800

    btn

--- js/music.js
+++ js/music.js
@@ -1,5 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var audio = document.getElementById("myAudio");
-    audio.play();
-  });
+// document.addEventListener("DOMContentLoaded", function() {
+//     var audio = document.getElementById("myAudio");
+//     audio.play();
+//   });
   
+
+document.addEventListener("DOMContentLoaded", function() {
+  var audio = document.getElementById("myAudio");
+  audio.load();
+  
+  var playButton = document.getElementById("playButton");
+  playButton.addEventListener("click", function() {
+    playAudio();
+  });
+});
+
+function playAudio() {
+  var myAudio = document.getElementById("myAudio");
+  myAudio.play();
+}

251f51985cfc8ebad935f25e1f5023994566f298
haze1109 <hazemorales11@gmail.com>
2023-04-15 17:54:03 +0800

    yes

--- js/darkmode-button.js
+++ js/darkmode-button.js
@@ -1,13 +1,18 @@
-
-// $("#dark-mode-button").on("click", function() {
-//     $(this).toggleClass("active");
-   
-//   });
-  // darkmode-button.js
-
+// script.js
 // Add event listener to toggle dark mode on click
 $("#dark-mode-button").on("click", function() {
     $(this).toggleClass("active");
     // Add logic to toggle dark mode, e.g. change styles, update classes, etc.
+  });
+  
+  function toggleDarkMode() {
+    var bodyElement = document.body;
+    bodyElement.classList.toggle("dark-mode"); // Toggle the dark mode class on body element
+  }
+  
+  var darkModeButton = document.getElementById("dark-mode-button");
+  
+  darkModeButton.addEventListener("click", function() {
+    toggleDarkMode(); // Call the toggleDarkMode() function when the dark mode button is clicked
   });
   

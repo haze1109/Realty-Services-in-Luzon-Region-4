0e668230726973e499111e876a5c03c91d04d672
haze1109 <hazemorales11@gmail.com>
2023-04-17 18:39:42 +0800

    js idesia modal

--- js/idesia_modal.js
+++ js/idesia_modal.js
@@ -1,41 +1,41 @@
 
-document.getElementById("showFormButton3").addEventListener("click", function () {
-    document.getElementById("googleFormContainer3").style.display = "block";
-  });
+// document.getElementById("showFormButton3").addEventListener("click", function () {
+//     document.getElementById("googleFormContainer3").style.display = "block";
+//   });
   
  
-  document.querySelector("#googleFormContainer3 .btn-close").addEventListener("click", function () {
-    document.getElementById("googleFormContainer3").style.display = "none";
-  });
+//   document.querySelector("#googleFormContainer3 .btn-close").addEventListener("click", function () {
+//     document.getElementById("googleFormContainer3").style.display = "none";
+//   });
   
  
-  document.addEventListener("click", function (event) {
-    if (event.target === document.getElementById("googleFormContainer3")) {
-      document.getElementById("googleFormContainer3").style.display = "none";
-    }
-  });
+//   document.addEventListener("click", function (event) {
+//     if (event.target === document.getElementById("googleFormContainer3")) {
+//       document.getElementById("googleFormContainer3").style.display = "none";
+//     }
+//   });
 
   // JavaScript code for the duplicated modal and button
 // Update the IDs in the code to match the duplicated modal and button IDs
 
 // Show modal when button is clicked
 document
-.getElementById("showFormButton4")
+.getElementById("showFormButton9")
 .addEventListener("click", function () {
-  document.getElementById("googleFormContainer4").style.display = "block";
+  document.getElementById("googleFormContainer9").style.display = "block";
 });
 
 // Close modal when close button is clicked
 document
-.querySelector("#googleFormContainer4 .btn-close")
+.querySelector("#googleFormContainer9 .btn-close")
 .addEventListener("click", function () {
-  document.getElementById("googleFormContainer4").style.display = "none";
+  document.getElementById("googleFormContainer9").style.display = "none";
 });
 
 // Close modal when clicked outside the modal
 document.addEventListener("click", function (event) {
-if (event.target === document.getElementById("googleFormContainer4")) {
-  document.getElementById("googleFormContainer4").style.display = "none";
+if (event.target === document.getElementById("googleFormContainer9")) {
+  document.getElementById("googleFormContainer9").style.display = "none";
 }
 });
 
